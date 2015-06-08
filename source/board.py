8dc1959c4b24588c68d5cbe5b34894ce6edd2277
Veke <veke.draganov@gmail.com>
2015-06-08 19:52:33 +0300

    modified:   source/board.py

--- source/board.py
+++ source/board.py
@@ -3,3 +3,12 @@ class Board(object):
     def __init__(self, width, height):
         self._width = width
         self._height = height
+        self.init_field_matrix(width, height)
+
+    def init_field_matrix(self, width, height):
+        self._fields = list[height]
+        for x in xrange(0, height):
+            self._fields[x] = list[width]
+
+    def get_field(self, x, y):
+        return self._fields[y][x]
